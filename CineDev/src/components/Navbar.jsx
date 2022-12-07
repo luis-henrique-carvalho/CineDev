0a2ba82b4a6d04851b99218e09f43dbf12979b49
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2022-12-07 17:53:01 -0300

    :lipstick: feat: Estilizaçao CSS da Navbar

--- CineDev/src/components/Navbar.jsx
+++ CineDev/src/components/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {BiCameraMovie, BiSearchAlt} from 'react-icons/bi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt } from "react-icons/bi";
+
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-        <h2>
-          <Link to='/'><BiCameraMovie/> CineDev</Link>
-        </h2>
-        <form>
-          <input type="text" placeholder='Busque um filme' />
-          <button type='submit'><BiSearchAlt/></button>
-        </form>
-      </nav>
-  )
-}
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> CineDev
+        </Link>
+      </h2>
+      <form>
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit">
+          <BiSearchAlt />
+        </button>
+      </form>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
